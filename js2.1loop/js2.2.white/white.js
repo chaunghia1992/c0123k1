26354ad21e9349243610fd16daf4532efb6334b6
chaunghia1992 <nchau0992@gmail.com>
2023-02-21 16:26:55 +0700

    white.html

---
+++ js2.1loop/js2.2.white/white.js
@@ -0,0 +1,22 @@
+/*
+let num = +prompt("enter a number:  ");
+let total = 0;
+while( num != -1 ) {
+
+    console.log(num);
+    total = total + num;
+    num = +prompt("Enter a number: ");
+
+}
+console.log("Total: " + total);
+
+*/
+
+
+let total = 0
+do {
+    number = +prompt("enter a number ");
+    total = total + number ;
+
+}while (number != -1 )
+console.log("total : " + total);
