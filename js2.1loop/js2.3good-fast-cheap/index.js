5fe6d56e6220a9f6cd3ae897b2e74b8ab1c1c57f
chaunghia1992 <nchau0992@gmail.com>
2023-02-21 17:04:45 +0700

    index.js

---
+++ js2.1loop/js2.3good-fast-cheap/index.js
@@ -0,0 +1,10 @@
+function changeGood() {
+    document.getElementById('fast').checked = !document.getElementById('good').checked;
+  }
+  function changeFast() {
+    document.getElementById('cheap').checked = !document.getElementById('fast').checked;
+  }
+  function changeCheap() {
+    document.getElementById('good').checked = !document.getElementById('cheap').checked;
+  }
+  
