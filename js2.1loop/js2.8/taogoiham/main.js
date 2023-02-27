4ff7b4a17531c57c1c5fd07fa78d35547de0179a
chaunghia1992 <nchau0992@gmail.com>
2023-02-27 16:01:40 +0700

    taogoiham.js

---
+++ js2.1loop/js2.8/taogoiham/main.js
@@ -0,0 +1,15 @@
+function processconfirm(answer){
+    let result = "";
+    if(ansewer){
+        result = " Excellent.we'll play a nice game off chess.";
+
+
+    } else {
+        result = "maybe latter then.";
+
+    }
+    return result;
+
+}
+let confirmanswer = confirm("shall we play a game?");
+let (theanswer);
